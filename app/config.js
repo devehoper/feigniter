beaa54fef3d520d4efd0e1e9cb13b5f2a836f97f
devehoper <devehoper@gmail.com>
2025-02-19 00:48:22 +0000

    Fixed virtual route. Now url can stay fixed.

--- app/config.js
+++ app/config.js
@@ -3,7 +3,7 @@ const config = {
     homeController:"HomeController",
     defaultMethod: "index",
     //if false url will be used in app.url
-    useNavigationBar: true,
+    useNavigationBar: false,
     //external libs to be added
     libs: [
         {
